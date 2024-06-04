08ea1a6e85653c1dc7ac21af2ecebe96991e13b0
IanEzeP <pereyra.ian00@gmail.com>
2024-06-04 18:02:30 -0300

    Audios de colores agregados, trabajando en el funcionamiento

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -11,26 +11,75 @@ import Swal from 'sweetalert2';
 })
 export class MainPage implements OnInit {
 
-  public buttonArray: Array<string> = new Array(5);
+  public buttonArray: Array<any> = new Array([5],[5],[5]);
+  public audioArray: Array<any> = new Array([5],[5],[5]);
+
+  audioSource: HTMLAudioElement = new Audio();
 
   constructor(private auth: AuthService, private router: Router, private alert: AlertService) { }
 
   ngOnInit() 
   {
-    this.buttonArray = ['../../assets/imagenes/colores/yellow.png',
+    let arrayColors = ['../../assets/imagenes/colores/yellow.png',
     '../../assets/imagenes/colores/green.png',
     '../../assets/imagenes/colores/blue.jpeg',
-    '../../assets/imagenes/colores/magenta.png',
+    '../../assets/imagenes/colores/purple.png',
     '../../assets/imagenes/colores/red.jpeg'];
+
+    let arrayAudios = ['assets/audios/english/colores/yellow.mp3',
+    'assets/audios/english/colores/green.mp3',
+    'assets/audios/english/colores/blue.mp3',
+    'assets/audios/english/colores/purple.mp3',
+    'assets/audios/english/colores/red.mp3'];
+    
+    this.buttonArray[0] = arrayColors;
+    this.audioArray[0] = arrayAudios;
+
+    let sf = new Audio();
   }
 
   //boton cambiar idioma
   //boton cambiar tematica
 
-  onBtnPrincipal()
+  async onBtnPrincipal(i: number)
   {
-    //Reproducir audio
-    //npm install @capacitor-community/native-audio No funcionó. Probar con web audio
+    console.log(this.audioSource.paused);
+    if(this.audioSource.paused) //Espera mucho...
+    {
+      try 
+      {
+        const assetCompleto = this.audioArray[0][i]; //CAMBIAR POR VARIABLE
+        
+        const audioElement = await this.loadAudio(assetCompleto);
+
+        if(!audioElement.paused) //Nunca se dispara
+        {
+          console.log(audioElement);
+          return;
+        }
+
+        audioElement.play().then(() => {
+          console.log('Audio reproduciendo...');
+        }).catch((error) => {
+          console.error('Error al reproducir ', error);
+        });
+      }
+      catch (error) 
+      {
+        console.error('Error al cargar ', error);
+      }
+    }
+    console.log(this.audioSource.paused);
+
+  }
+
+  async loadAudio(url: string): Promise<HTMLAudioElement>
+  {
+    return new Promise((resolve, reject) => {
+      this.audioSource.src = url;
+      this.audioSource.addEventListener('canplaythrough', () => resolve(this.audioSource));
+      this.audioSource.onerror = () => reject(new Error('Error loading audio: ' + url));
+    })
   }
 
   cerrarSesion()
